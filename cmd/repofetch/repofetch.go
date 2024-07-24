851c9e8b5a8d55ecc0c370f8cf97f22654ee7109
Daniel Antonsen <44880186+root4loot@users.noreply.github.com>
2024-07-24 17:20:03 +0200

    Add support for gh auth
    - Added GitHub CLI auth support
    - Refactor auth logic
    - Updated flags and usage

--- cmd/repofetch/repofetch.go
+++ cmd/repofetch/repofetch.go
@@ -3,99 +3,146 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
-	"log/slog"
 	"os"
+	"os/exec"
+	"strings"
+
+	"log/slog"
 
 	"github.com/supply-chain-tools/go-sandbox/gitkit"
 )
 
 const usage = `Usage:
-    repofetch [options] <path>
+  repofetch [options] <path>...
 
 Options:
-    -d, -debug    Enable debug logging
-    -h, -help     Show help message
+  --gh-auth    Use GitHub CLI for authentication
+  --debug      Enable debug logging
+  -h, --help   Display help
 
 Environment Variables:
-    GITHUB_TOKEN  GitHub token for authenticated requests
+  GITHUB_TOKEN  GitHub token (optional)
 
-Currently, only the path prefix 'github.com/' is supported
+Notes:
+  If no token is provided, repositories will be cloned unauthenticated.
 
 Examples:
-    Fetch all repos for one org/user:
-        $ repofetch github.com/torvalds
+  Fetch all repos for one org/user:
+    $ repofetch github.com/torvalds
 
-    Fetch one repo:
-        $ repofetch github.com/torvalds/linux`
+  Fetch one repo:
+    $ repofetch github.com/torvalds/linux`
 
 func main() {
-	flag.Usage = func() {
-		fmt.Println(usage)
+	useGitHubAuth, debug, args := parseFlagsAndArgs()
+
+	logLevel := slog.LevelInfo
+	if debug {
+		logLevel = slog.LevelDebug
 	}
 
-	flags := flag.NewFlagSet("all", flag.ExitOnError)
-	var help, h, debugMode bool
-	flags.BoolVar(&help, "help", false, "")
-	flags.BoolVar(&h, "h", false, "")
-	flags.BoolVar(&debugMode, "debug", false, "")
-
-	err := flags.Parse(os.Args[1:])
-	if err != nil || help || h {
-		fmt.Println(usage)
-		os.Exit(1)
+	logOptions := &slog.HandlerOptions{
+		Level: logLevel,
 	}
-
-	opts := &slog.HandlerOptions{
-		Level: slog.LevelInfo,
-	}
-
-	if debugMode {
-		opts.Level = slog.LevelDebug
-	}
-
-	logger := slog.New(slog.NewTextHandler(os.Stdout, opts))
+	logger := slog.New(slog.NewTextHandler(os.Stdout, logOptions))
 	slog.SetDefault(logger)
 
-	var client *gitkit.GitHubClient
-	token, err := getGitHubToken()
+	token, err := getToken(useGitHubAuth)
 	if err != nil {
-		slog.Debug(fmt.Sprintf("%s. Using unauthenticated client.", err.Error()))
-		client = gitkit.NewGitHubClient()
-	} else {
-		slog.Debug("GitHub token found")
-		client = gitkit.NewAuthenticatedGitHubClient(token)
+		slog.Debug("Failed to get token", "error", err)
 	}
 
-	localBasePath, err := os.Getwd()
+	client, err := createGitHubClient(token)
 	if err != nil {
-		log.Fatal(err)
+		slog.Debug("Failed to create GitHub client", "error", err)
 	}
 
-	args := flags.Args()
-	if len(args) == 0 {
-		fmt.Println(usage)
+	if err := fetchRepositories(client, args); err != nil {
+		slog.Debug("Failed to fetch repositories", "error", err)
 		os.Exit(1)
-	}
-
-	for _, arg := range args {
-		owner, repoName, err := gitkit.ExtractOwnerAndRepoName(arg)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		err = client.CloneOrFetchAllRepos(owner, repoName, localBasePath)
-		if err != nil {
-			log.Fatal(err)
-		}
 	}
 }
 
-func getGitHubToken() (string, error) {
-	const envVarName = "GITHUB_TOKEN"
-	token := os.Getenv(envVarName)
+func getToken(useGitHubAuth bool) (token string, err error) {
+	if useGitHubAuth {
+		token, err = getTokenFromCLI()
+		if err != nil {
+			return "", fmt.Errorf("failed to get token from CLI: %w", err)
+		}
+	} else {
+		token, err = getTokenFromEnv("GITHUB_TOKEN")
+		if err != nil {
+			return "", fmt.Errorf("failed to get token from environment: %w", err)
+		}
+	}
+	return token, err
+}
+
+func getTokenFromEnv(name string) (string, error) {
+	token := os.Getenv(name)
 	if token == "" {
-		return "", fmt.Errorf("GitHub token not set in environment variable %s", envVarName)
+		return "", fmt.Errorf("could not get token from environment variable %s", name)
 	}
 	return token, nil
+}
+
+func getTokenFromCLI() (string, error) {
+	cmd := exec.Command("gh", "auth", "token")
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", fmt.Errorf("error executing GitHub CLI `gh auth token`: %s, details: %w", strings.TrimSpace(string(output)), err)
+	}
+	return strings.TrimSpace(string(output)), nil
+}
+
+func createGitHubClient(token string) (*gitkit.GitHubClient, error) {
+	if token == "" {
+		slog.Debug("Using unauthenticated client")
+		return gitkit.NewGitHubClient(), nil
+	}
+	slog.Debug("Using authenticated client")
+	return gitkit.NewAuthenticatedGitHubClient(token), nil
+}
+
+func parseFlagsAndArgs() (bool, bool, []string) {
+	var ghAuth, debug bool
+
+	flag.Usage = func() {
+		fmt.Fprintln(os.Stderr, usage)
+	}
+
+	help := flag.Bool("help", false, "")
+	flag.BoolVar(help, "h", false, "")
+	flag.BoolVar(&debug, "debug", false, "")
+	flag.BoolVar(&ghAuth, "gh-auth", false, "")
+
+	flag.Parse()
+	args := flag.Args()
+
+	if *help || len(args) == 0 {
+		flag.Usage()
+		os.Exit(0)
+	}
+
+	return ghAuth, debug, args
+}
+
+func fetchRepositories(client *gitkit.GitHubClient, paths []string) error {
+	dir, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+
+	for _, path := range paths {
+		owner, repoName, err := gitkit.ExtractOwnerAndRepoName(path)
+		if err != nil {
+			return fmt.Errorf("failed to extract owner and repo name for path %s: %w", path, err)
+		}
+
+		if err := client.CloneOrFetchAllRepos(owner, repoName, dir); err != nil {
+			return fmt.Errorf("failed to clone or fetch repos for %s/%s: %w", owner, *repoName, err)
+		}
+	}
+
+	return nil
 }

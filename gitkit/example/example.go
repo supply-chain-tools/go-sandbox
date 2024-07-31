cf8920fdc852b1bd97b3e839b90c89b33242d120
Daniel Antonsen <44880186+root4loot@users.noreply.github.com>
2024-07-31 20:41:11 +0200

    Refactor gitkit, update CLI, add example.go
    Gitkit:
    - Refactor clone functions to simplify API and clarity
    - Decoupled concurrency handling from lib as API changes allow this to be managed at outer layer
    - Cloning function now return `CloneResult` which also contain errors to track failed clones
    - Support `io.Writer` option for polling clone/fetch progress
    - Added `example.go`
    
    CLI:
    - Now manages concurrency

---
+++ gitkit/example/example.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"time"
+
+	"github.com/supply-chain-tools/go-sandbox/gitkit"
+)
+
+func main() {
+	repoPaths := []string{
+		"github.com/torvalds", // all repos for user/org
+		"https://github.com/golang/go",
+		"github.com/kubernetes/kubernetes.git",
+	}
+
+	client := gitkit.NewGitHubClient()
+
+	dir, err := os.Getwd()
+	if err != nil {
+		fmt.Printf("Error getting current directory: %v\n", err)
+		return
+	}
+
+	var outputBuffer bytes.Buffer
+	progressWriter := io.Writer(&outputBuffer)
+
+	cloneOpts := gitkit.CloneOptions{
+		// Depth:    1,
+		Bare:     false,
+		Progress: &progressWriter,
+	}
+
+	// monitor output buffer and print updates
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case <-done:
+				return
+			default:
+				if outputBuffer.Len() > 0 {
+					fmt.Print(outputBuffer.String())
+					outputBuffer.Reset() // clear buffer on print
+				}
+				time.Sleep(20 * time.Millisecond) // avoid busy loop
+			}
+		}
+	}()
+
+	for _, path := range repoPaths {
+		repos, err := client.GetRepositories(path)
+		if err != nil {
+			fmt.Printf("Failed to list repositories for path %s: %v\n", path, err)
+			continue
+		}
+
+		for _, repoURL := range repos {
+			fmt.Printf("Cloning repository: %s\n", repoURL)
+			result, err := client.CloneOrFetchRepo(repoURL, dir, cloneOpts)
+			if err != nil {
+				fmt.Printf("Error cloning repository %s: %v\n", repoURL, err)
+			} else {
+				fmt.Println("Result:", result)
+			}
+		}
+
+		done <- true
+		time.Sleep(100 * time.Millisecond) // wait for last print
+	}
+}
